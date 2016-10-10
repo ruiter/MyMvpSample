2a7117b2d1bb4c2fa64f52c3533485cc7d66778a
ruiter <ruiter@ruiter>
2016-10-10 11:05:45 -0300

    finalizing registration

--- app/src/main/java/br/com/ruitercomp/mymvpsample/RegisterActivity.java
+++ app/src/main/java/br/com/ruitercomp/mymvpsample/RegisterActivity.java
@@ -28,7 +28,9 @@ public class RegisterActivity extends AppCompatActivity implements RegisterView 
         findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                registerPresenter.validateRegister(name.getText().toString(),
+                        username.getText().toString(),
+                        password.getText().toString());
             }
         });
 
